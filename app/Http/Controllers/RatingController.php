950a5232aa4e51f1ae5e0cdf776f5340740d25a4
Dmytro Shparvasser <dmytro.shparvasser@codeit.pro>
2021-12-27 12:10:43 +0200

    fixed rating controller

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Rating;
+use App\Http\Requests\RatingRequest;
 
 class RatingController extends Controller
 {
     public function ratingOn(RatingRequest $req, $id)
     {
         $rating = new Rating();
-        // $rating->author_id = $req->user()->id;
         $rating->author_id = $req->input('author_id');
         $rating->rating = $req->input('rating');
-        // $rating->book_id = $id;
         $rating->book_id = $req->input('book_id');
         $rating->save();
         return redirect()->route('home')->with('success', 'Your rating added');

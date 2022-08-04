31bec4f6319bf3e1395cc2f4f6c6f3132bee1815
Dima Shparvasser <stalkerok3000@gmail.com>
2022-08-04 17:48:04 +0300

    d

--- resources/js/swe.js
+++ resources/js/swe.js
@@ -3,6 +3,12 @@ var translations = {
         "one": "One user online.",
         "other": "%i users online."
     },
-    "simple_key": "Hi, I am a key!"
+    "simple_key": "Hi, I am a key!",
+    "online_store_url": {
+        "url": 'The online store URL format is invalid.',
+        "required": 'The online store URL field is required.',
+        "customRegex": 'The online store URL field may contain only Latin letters, numbers and hyphens',
+        "max": "The online store URL may not be greater than {{args}} characters.",
+    },
 };
 
